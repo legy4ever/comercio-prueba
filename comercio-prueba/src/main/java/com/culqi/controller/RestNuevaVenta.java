4bcdb68707f12194812b52daebe0e0f899fa6ae3
Luis Gonzales <luis.gonzales@culqi.com>
2018-04-24 22:11:54 -0500

    Updates:
    - Prueba: lectura de la respuesta del API

--- comercio-prueba/src/main/java/com/culqi/controller/RestNuevaVenta.java
+++ comercio-prueba/src/main/java/com/culqi/controller/RestNuevaVenta.java
@@ -144,12 +144,16 @@ public class RestNuevaVenta {
             log.info("Respuesta de la venta: " + responseVenta.toString());
 
             object = new JSONObject(responseVenta.getBody());
+            OutComeData outComeData = null;
 
-            test = object.get("outcome").toString();
+            if (object.get("object").equals("charge")) {
+                test = object.get("outcome").toString();
+                outComeData = new Gson().fromJson(test, OutComeData.class);
+            } else {
+                test = object.get("user_message").toString();
+            }
 
-            OutComeData outComeData = new Gson().fromJson(test, OutComeData.class);
-
-            return ResponseEntity.ok(outComeData != null ? outComeData.getUserMessage() : test);
+            return ResponseEntity.ok(outComeData != null ? outComeData.getUserMessage() != null ? outComeData.getUserMessage() : test : test);
 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error en la venta. Por favor comuníquese con CULQI para más información.");
